eb431504b0c86d09360fb2003319f595de277d62
victorschmidt21 <naoseivtx21@gmail.com>
2024-10-07 21:09:30 -0300

    LEal

--- schemaTypes/testetype.ts
+++ schemaTypes/testetype.ts
@@ -2,13 +2,28 @@ import {defineField, defineType} from 'sanity'
 
 export const testeType = defineType({
   name: 'event',
-  title: 'Nome',
+  title: 'Cadastro',
   type: 'document',
   fields: [
     defineField({
       name: 'nome',
       type: 'string',
     }),
-   
+    defineField({
+        name: 'sobrenome',
+        type: 'string',
+    }),
+    defineField({
+        name: 'Idade',
+        type: 'number', 
+    }),
+    defineField({
+        name: 'imagem',
+        type: 'image',
+    }),
+    defineField({
+        name: 'Viado',
+        type: 'boolean',
+    })
   ],
 })
